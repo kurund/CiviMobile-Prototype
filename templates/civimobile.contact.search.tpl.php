5f732c1b6abc8c3f3dd12f18eafeb667f8ecc00d
Erawat <erawat@gmail.com>
2012-03-01 15:17:22 +0000

    add postcode search functionality

--- templates/civimobile.contact.search.tpl.php
+++ templates/civimobile.contact.search.tpl.php
@@ -1,13 +1,12 @@
 <?php
     $url = (!empty($_SERVER['HTTPS'])) ? "https://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'] : "http://".$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
     $parse_url = parse_url($url, PHP_URL_PATH);
-    
      include('civimobile.header.php');
 ?>
 <div data-role="page" data-theme="c" id="jqm-contacts">
 
   <div id="jqm-contactsheader" data-role="header">
-        <h3>Search Contacts</h3>
+        <h3>Contacts Location Search</h3>
         <a href="/civimobile" data-ajax="false" data-direction="reverse" data-role="button" data-icon="home" data-iconpos="notext" class="ui-btn-right jqm-home">Home</a>
 		<!-- <a href="#" id="add-contact-button" data-role="button" data-icon="plus" class="ui-btn-left jqm-home">Add</a> 
 		<a href="#" id="back-contact-button" data-role="button" data-icon="arrow-l" class="ui-btn-left jqm-home" style="display:none">Back</a>
@@ -15,17 +14,15 @@
  </div>
  
 <div data-role="content" id="contact-content"> 
-  	<div class="ui-listview-filter ui-bar-c">
-   <div data-role="fieldcontain" class="ui-hide-label">
-      <label for="postcode">postcode:</label>
-      <input type="text" name="postcode" id="postcode" value="" placeholder="Postcode"/>
-  </div>
+  <div class="ui-listview-filter ui-bar-c">
+  <input type="search" name="postcode" id="postcode" placeholder="Postcode"/>
  	 <div data-role="fieldcontain">
-			<input type="checkbox" name="curLocation-1" id="useLocation" class="custom" />
+			<input type="checkbox" name="curLocation" id="useLocation" class="custom" />
 			<label for="useLocation">Use current location</label>
-	    </fieldset>
 	 </div>   
+
 	 </div>
+
  </div>
 
 
@@ -34,40 +31,76 @@
   </div>  
  
     	  <script language="javascript" type="text/javascript">
-
+            
     		   	$(document).ready(function(){
-
+              alert(crmajaxURL);
      				// Add a click listener on the button to get the location data
-     				$('#useLocation').click(function(){
-   					 if (this.checked) {
-          				if (navigator.geolocation) {
-               				navigator.geolocation.getCurrentPosition(onSuccess, onError);
-          				} else {
-               				// If location is not supported on this platform, disable it
-               				$('#useLocation').value = "Geolocation not supported";
-               				$('#useLocation').unbind('click');
-          				}
-          		}else{
-                $('#contacts').remove()
-              }
-     				});
+       				$('#useLocation').click(function(){
+              $('#postcode').val('');
+     					 if (this.checked) {
+            				if (navigator.geolocation) {
+                 				navigator.geolocation.getCurrentPosition(onSuccess, onError);
+            				} else {
+                 				// If location is not supported on this platform, disable it
+                 				$('#useLocation').value = "Geolocation not supported";
+                 				$('#useLocation').unbind('click');
+                        //$('#postcode').show();
+            				}
+            		}else{
+                  $('#contacts').remove();
+                }
+     				 });
 
-     				function onSuccess(location) {
-						//var message = document.getElementById("locationResult");
-						//message.innerHTML ="<img src='http://maps.google.com/staticmap?center=" + location.coords.latitude + "," + location.coords.longitude + "&size=300x200&maptype=hybrid&zoom=16&key=YOURGOOGLEAPIKEY' />";						
-						console.log('latitude' + location.coords.latitude );
-						console.log('longitude' + location.coords.longitude );
-						console.log('Accuracy: ' + location.coords.accuracy);
-						searchContactByGeoLocation(location);
+             $('#postcode').change (function () {
+                $('#useLocation').attr('checked', false);    
+                $("#useLocation").checkboxradio("refresh");    
+                $('#contacts').remove();
+                searchContactByPostcode ($(this).val());
+              }); 
+
+     				function onSuccess(location) {  			
+  						console.log('latitude' + location.coords.latitude );
+  						console.log('longitude' + location.coords.longitude );
+  						console.log('Accuracy: ' + location.coords.accuracy);
+  						searchContactByGeoLocation(location);
 					}
      				 
 					// Error function for Geolocation call
 					function onError(msg){
 						$('#locationResult').html("Geolocation not supported");
-	  					$('#useLocation').unbind('click');
+	  				$('#useLocation').unbind('click');
 					}
  
 				});
+
+function searchContactByPostcode(postcode){
+      $.mobile.showPageLoadingMsg( 'Searching' );
+      console.log(postcode)
+      if(postcode != null){
+        $().crmAPI ('Address','get',{'version' :'3', 'postal_code' : postcode}
+          ,{
+           ajaxURL: crmajaxURL,
+           success:function (data){ 
+            if(data.count != 0){
+              $('#contact-content').append('<ul id="contacts" data-role="listview" data-inset="false" data-filter="false" ></ul>');
+              $.each(data.values, function(key, value) {
+                if(value.contact_id != null){
+                  $().crmAPI ('Contact','getsingle',{'version' :'3', 'contact_id' :value.contact_id}
+                      ,{ 
+                       ajaxURL: crmajaxURL,
+                       success:function (data){    
+                          buildList(data);
+                          $('#contacts').listview();
+                       }
+                  });
+                }
+              });
+            }  
+          }
+        }); 
+      }     
+      $.mobile.hidePageLoadingMsg( );
+}        
 
 function searchContactByGeoLocation (params){
     $.mobile.showPageLoadingMsg( 'Searching' );
@@ -75,24 +108,24 @@ function searchContactByGeoLocation (params){
           ,{ 
             ajaxURL: crmajaxURL,
             success:function (data){ 
-              if (data.count == 0) {
-                cmd = null;
-                $('#contacts').hide();                                             
+              if (data.count == 0) {                
+                 $('#contacts').hide();    
               }
               else {
-                cmd = "refresh";
-                $('#contacts').show();
-                $('#contacts').empty();
+                $('#contact-content').append('<ul id="contacts" data-role="listview" data-inset="false" data-filter="false" ></ul>');
+                $.each(data.values, function(key, value) {
+                    buildList(value);
+                });
+                $('#contacts').listview();
               }
-
-              $('#contact-content').append('<ul id="contacts" data-role="listview" data-inset="false" data-filter="false" ></ul>');
-  			      $.each(data.values, function(key, value) {
-     			      $('#contacts').append('<li role="option" tabindex="-1" data-ajax="false" data-theme="c" id="contact-'+value.contact_id+'" ><a href="/codesprint/civimobile/contact/'+value.contact_id+'" data-role="contact-'+value.contact_id+'">'+value.display_name+'</a></li>');
-   			      });
-              $.mobile.hidePageLoadingMsg( );
-              $('#contacts').listview();
+              $.mobile.hidePageLoadingMsg( );             
           }
    });
+}
+
+
+function buildList(data){
+  $('#contacts').append('<li role="option" tabindex="-1" data-ajax="false" data-theme="c" id="contact-'+data.contact_id+'" ><a href="/codesprint/civimobile/contact/'+data.contact_id+'" data-role="contact-'+data.contact_id+'">'+data.display_name+'</a></li>');
 }
 	
         
